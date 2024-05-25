e056e6f014e939f42a0bb7fbbc729b24dc25bcc6
Spandana GM <spandanagm23@gmail.com>
2024-05-25 11:43:53 +0530

    1st Commit

---
+++ hello.py
@@ -0,0 +1,23 @@
+# print("Hello World")
+# print(2,3)
+# pas='a'
+# print(pas)
+# s=9
+# b=10
+# diff=s-b
+# print(diff)
+# str="*"
+# print(2*str*3)
+# a=9
+# b=3
+# print(a//b)
+# a=-5
+# b=-3
+# print(a%b)
+age=int(input("age:"))
+if(age<18):
+    print("not adult")
+elif(age==18):
+    print("just adult")
+else:
+    print("adult")
